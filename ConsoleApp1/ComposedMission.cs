0872f1d6a413ae1a9a887791681f4b4a19ef6b7c
Eyal <eyalcohen308@gmail.com>
2019-03-24 13:37:57 +0200

    added docs to the funcions

--- ConsoleApp1/ComposedMission.cs
+++ ConsoleApp1/ComposedMission.cs
@@ -7,25 +7,36 @@ using System.Threading.Tasks;
 namespace Excercise_1
 {
     /**
-     * 
-     * 
+     * class that calculate complex calculations base on given double.
      * */
     public class ComposedMission : IMission
     {
+        // event handler that sumup all the events that happened.
         public event EventHandler<double> OnCalculate;
+        // list of function to calculate base on an input.
         List<Delegate1> functions;
+        // name of specific instant object of the class.
         string name;
-
+        
+        // constructor only by name of object.
         public ComposedMission(string name)
         {
             this.name = name;
             functions = new List<Delegate1>(); 
         }
+
+        /**
+         * add functions to the list of functions to calculate.
+         * the function return same type to manage multy functions on the same object.
+         **/
         public ComposedMission Add(Delegate1 func)
         {
             functions.Add(func);
             return this;
         }
+        /**
+         * properties of name and type
+         * */
         public string Name
         {
             get
@@ -33,6 +44,7 @@ namespace Excercise_1
                 return name;
             }
         }
+        
         public string Type
         {
             get
@@ -40,6 +52,11 @@ namespace Excercise_1
                 return ("Composed");
             }
         }
+
+        /**
+         * calculate the functions one by one, one's output is another's input.
+         * check if the event handler already initilize, if so add the result and the sender to it.
+         * */
         public double Calculate(double d)
         {
             double result = d;
--- ConsoleApp1/IMission.cs
+++ ConsoleApp1/IMission.cs
@@ -6,6 +6,9 @@ using System.Threading.Tasks;
 
 namespace Excercise_1
 {
+    /**
+     * Interface of a mission. must have event handler and properties of name and type.
+     * */
     public interface IMission
     {
         event EventHandler<double> OnCalculate;  // An Event of when a mission is activated
